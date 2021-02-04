9d3305823d2d95cd1ba93d18df4146acb565c985
Reflex2468 <xavier.coppens@digitalmove.be>
2021-02-04 16:43:28 +0100

    Setup testing

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace juniorE\ShoppingCart\Tests\Feature;
+
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function exampleTest()
+    {
+        $this->assertTrue(true);
+    }
+}
